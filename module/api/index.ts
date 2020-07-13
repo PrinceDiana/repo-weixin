6ce29f963f73df7497dfff31bb7f937231e3b37b
qixiong.li 李其雄 <qixiong.li@qiyuesuo.com>
2020-07-13 14:38:55 +0800

    feature/B test two repos commit

--- module/api/index.ts
+++ module/api/index.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 // 父仓库修改子模块
 // 来自repo-weixin/feature/B的提交
+// 来自repo-weixin/feature/B的提交 2
 export class ApiContractBatch {
     static queryBatchList(params) {
         return axios.get('/batchsign/query', { params });
