b0fc2a6456d7d3eb266b9d5adc601ba7a803fd85
qixiong.li 李其雄 <qixiong.li@qiyuesuo.com>
2020-07-19 17:43:48 +0800

    Squashed 'shared/' content from commit 2f5ca63
    git-subtree-dir: shared
    git-subtree-split: 2f5ca63e82898911d270ddcaf101a51ee0cef251

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+// 父仓库修改子模块
+// 来自repo-weixin/feature/B的提交
+export class ApiContractBatch {
+    static queryBatchList(params) {
+        return axios.get('/batchsign/query', { params });
+    }
+}
