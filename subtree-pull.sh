c9376b4dde953477cfa1e7ea7c7e2e8fc4503ac0
qixiong.li 李其雄 <qixiong.li@qiyuesuo.com>
2020-07-19 17:43:46 +0800

    workflow: sh调整

--- subtree-pull.sh
+++ subtree-pull.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 当前分支
-branch = `git branch | grep "*"`
+$branch = `git branch | grep "*"`
 
 # 拉取子仓库
-git subtree pull --prefix=module https://github.com/PrinceDiana/repo-common.git branch
+git subtree pull --prefix=shared ../repo-common $branch
--- subtree-push.sh
+++ subtree-push.sh
@@ -1,8 +1,8 @@
 
 #!/bin/bash
 # 当前分支
-branch = `git branch | grep "*"`
+$branch = `git branch | grep "*"`
 
 # 推送子仓库
-git subtree push --prefix=module https://github.com/PrinceDiana/repo-common.git branch
+git subtree push --prefix=shared ../repo-common $branch
 
