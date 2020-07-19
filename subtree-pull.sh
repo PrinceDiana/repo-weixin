3cd557fb5f244b201e6dba10e9e7191dc1b90dac
qixiong.li 李其雄 <qixiong.li@qiyuesuo.com>
2020-07-19 17:49:36 +0800

    sh

--- subtree-pull.sh
+++ subtree-pull.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # 当前分支
-$branch = `git branch | grep "*"`
+branch=`git branch | grep "*"`
+
+echo branch
 
 # 拉取子仓库
-git subtree pull --prefix=shared ../repo-common $branch
+git subtree pull --prefix=shared ../repo-common branch
