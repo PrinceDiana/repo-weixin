e3a8dcf1ed850ca02806811f6c56361132032e09
qixiong.li 李其雄 <qixiong.li@qiyuesuo.com>
2020-07-20 16:30:28 +0800

    Squashed 'shared/' changes from c4c2168..6db83da
    6db83da REQ-2236
    f64ef89 REQ-1225
    ce12477 接口response
    3fffe16 Merge branch 'feature/B' into hotfix/4.1.x
    c242816 父子仓库同时修改
    
    git-subtree-dir: shared
    git-subtree-split: 6db83dab5c97ba53e209f3d0d86f35b3c9bc8eba

--- model/index.ts
+++ model/index.ts
@@ -1,5 +1,16 @@
+// 接口response
 export interface ResponseResult {
     code: number
     message: string
     [key: string]: any
+}
+
+// REQ-1225
+export interface Contract {
+    id: string
+}
+
+// REQ-2236
+export interface Seal {
+    id: string
 }
